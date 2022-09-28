fe6a0d622058db256f29b6c44769a7277cd04c5a
Eugene <decadence127@gmail.com>
2022-09-28 18:02:22 +0300

    arch: initialize file microservice

--- microservies_workspace/file_service/index.ts
+++ microservies_workspace/file_service/index.ts
@@ -4,11 +4,11 @@ import bodyParser from "body-parser";
 
 const app: express.Application = express();
 
-const PORT = process.env.AUTH_SERVICE_PORT ?? 5006;
+const PORT = process.env.FILE_SERVICE_PORT ?? 5007;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Auth service has been started on ${PORT} port.`);
+  console.log(`File service has been started on ${PORT} port.`);
 });
